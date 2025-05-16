69e147d8c0d3bd47fb0df1b4e1a860f6e2da2abe
volodymyr <vovanose@ukr.net>
2025-05-16 16:37:54 +0300

    Add task 02_3 Convert decimal to binary, octo, hex

---
+++ HomeWorkCSpro_02_3_Struct_toConvertDecimal_2_8_16/DecimalNumber.cs
@@ -0,0 +1,31 @@
+﻿namespace HomeWorkCSpro_02_3_Struct_toConvertDecimal_2_8_16
+{
+    struct DecimalNumber
+    {
+        private int number;
+        public DecimalNumber(int number)
+        {
+            this.number = number;
+        }
+        public string ToBinary()
+        {
+            return Convert.ToString(number, 2);
+        }
+        public string ToOctal()
+        {
+            return Convert.ToString(number, 8);
+        }
+        public string ToHexadecimal()
+        {
+            return Convert.ToString(number, 16).ToUpper();
+        }
+
+        public void OutputConsole()
+        {
+            Console.WriteLine($"Десяткове число: {number}");
+            Console.WriteLine($"Двійкова система: {ToBinary()}");
+            Console.WriteLine($"Вісімкова система: {ToOctal()}");
+            Console.WriteLine($"Шістнадцяткова система: {ToHexadecimal()}");
+        }
+    }
+}
---
+++ HomeWorkCSpro_02_3_Struct_toConvertDecimal_2_8_16/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace HomeWorkCSpro_02_3_Struct_toConvertDecimal_2_8_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            for (int i = 5; i < 30; i += 10)
+            {
+                new DecimalNumber(i).OutputConsole();
+                Console.WriteLine("-----------------------------");
+            }
+        }
+    }
+}
